44853361bec2534a99c6d38786c310c579bf178d
TESSOH TENE <michel.tessohtene@cgi.com>
2025-04-01 10:25:39 +0200

    first commit

---
+++ src/main/java/com/spring_batch_3/SpringBatch3Application.java
@@ -0,0 +1,13 @@
+package com.spring_batch_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_batch_3/SpringBatch3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_batch_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
